17721f23903abf6f0e4db2cb84b7e8543cbc5354
Michael Hopper <drmikeh@gmail.com>
2019-01-08 10:42:07 -0500

    Redux: adds redux-logger middleware.

--- redux/src/app/redux/configureStore.js
+++ redux/src/app/redux/configureStore.js
@@ -1,11 +1,19 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, compose, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from './root-reducer';
 import { fetchTodos } from '../../todos/redux/actions';
 
 const configureStore = () => {
+
+    const middlewares = [thunk];
+    if (process.env.NODE_ENV === 'development') {
+        const { logger } = require('redux-logger');
+        middlewares.push(logger);
+    }
+
     // create and populate the Redux store
-    const store = createStore(rootReducer, applyMiddleware(thunk));
+    // const store = createStore(rootReducer, applyMiddleware(thunk, logger));
+    const store = compose(applyMiddleware(...middlewares))(createStore)(rootReducer);
     store.dispatch(fetchTodos());
     return store;
 }
