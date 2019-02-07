9667184b81b615ec8057652e0ce0b5a0dce8b13c
Michael Hopper <drmikeh@gmail.com>
2019-02-07 10:57:54 -0600

    Adds integration testing to set-state

--- clients/set-state/src/components/app/App.test.js
+++ clients/set-state/src/components/app/App.test.js
@@ -1,9 +1,41 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { mount } from 'enzyme';
+import { expect } from 'chai';
 import App from './App';
+import Todo from '../todos/Todo';
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+  mount(<App />);
+});
+
+it('renders todos title', () => {
+  const wrapper = mount( <App /> );
+  expect(wrapper.find('h1').text()).to.equal('todos');
+});
+
+it('renders 3 todos', () => {
+  const wrapper = mount( <App /> );
+  
+  // We need jest to wait until the axios data arrives, so we return a Promise that
+  // isn't resolved until after 500ms. It's a hack but I could not find a better way
+  // to do an integration test.
+  return new Promise((resolve, reject) => {    
+    setTimeout(() => {
+      function verifyTodo(wrapper, expectedTitle, expectedCompleted) {
+        expect(wrapper.text()).to.equal(expectedTitle);
+        expect(wrapper.prop('todo').title).to.equal(expectedTitle);
+        expect(wrapper.prop('todo').completed).to.equal(expectedCompleted);
+        expect(wrapper.childAt(0).hasClass('completed')).to.equal(expectedCompleted);
+      }
+      wrapper.update();
+      expect(wrapper.find(Todo).length).to.equal(3);
+
+      // console.log(learnReact.debug());
+      verifyTodo(wrapper.find(Todo).at(0), 'Learn React', true);
+      verifyTodo(wrapper.find(Todo).at(1), 'Learn Redux', true);
+      verifyTodo(wrapper.find(Todo).at(2), 'Learn GraphQL', false);
+
+      resolve('all done');
+    }, 1000);
+  });
 });
--- servers/rest/app.js
+++ servers/rest/app.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const cors = require('cors');
 const todosRouter = require('./routes/todos');
 
 const app = express();
+app.use(cors());
 
 if (process.env.NODE_ENV !== 'test') {
     app.use(logger('dev'));
